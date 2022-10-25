332f2f8c3ca78ad80fe447d6edb3b8e31d36138a
Mikołaj Herzberg <mikolaj.herzberg@gmail.com>
2022-10-25 22:14:14 +0200

    Rejestrowanie uzytkownikow - do sesji serwera

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 from google.cloud import datastore
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 auth = HTTPBasicAuth()
-datastore_client = datastore.Client()
+# datastore_client = datastore.Client()
 
 users = {}
 
 
-@app.route('/register')
-def register(username, password):
+@app.route('/register', methods=['POST'])
+@cross_origin()
+def register():
+    print(request.args)
+    username = request.json['email']
+    password = request.json['password']
     if username in users:
-        return 409
+        return "There is a user with this email", 409
     else:
-        user = {}
-        user.username = username
-        user.password = generate_password_hash(password)
-        pass
+        user = {username: username, password: generate_password_hash(password)}
+        users[user[username]] = user[password]
+        return users
 
 
 @app.route('/')
