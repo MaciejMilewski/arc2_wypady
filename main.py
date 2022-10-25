ccb9f129781fd581c59251c2cc67b4c3b97c6ea9
Maciej Milewski <macias.argedius@gmail.com>
2022-10-25 21:03:33 +0200

    Auth in datastore - Work in progress

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import os
 
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+from google.cloud import datastore
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+datastore_client = datastore.Client()
+
+users = {}
+
+
+@app.route('/register')
+def register(username, password):
+    if username in users:
+        return 409
+    else:
+        user = {}
+        user.username = username
+        user.password = generate_password_hash(password)
+        pass
 
 
 @app.route('/')
