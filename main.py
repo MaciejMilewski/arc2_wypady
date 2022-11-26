1d1476e5650cdba5ba77e444ef6bd88849406b54
Maciej Milewski <macias.argedius@gmail.com>
2022-11-26 16:18:12 +0100

    Added explicit content check

--- main.py
+++ main.py
@@ -152,6 +152,17 @@ def add_new_food():
     description = request.form.get("description")
     image = request.files['file']
 
+    # Storage bucket
+    # Create a Cloud Storage client.
+    gcs = storage.Client()
+    #
+    # Get the bucket that the file will be uploaded to.
+    bucket = gcs.get_bucket("staging.wypady.appspot.com")
+    blob = bucket.blob("restaurants/" + image.filename)
+    blob.upload_from_string(
+        image.read(),
+        content_type=image.content_type)
+
     # pub sub
     publisher = pubsub_v1.PublisherClient()
     topic_path = 'projects/wypady/topics/isImageFood'
@@ -176,17 +187,6 @@ def add_new_food():
         menu['description'] = description
         menu['price'] = price
         menu['restaurantKey'] = restaurant_name_key
-
-        # Storage bucket
-        # Create a Cloud Storage client.
-        gcs = storage.Client()
-        #
-        # Get the bucket that the file will be uploaded to.
-        bucket = gcs.get_bucket("staging.wypady.appspot.com")
-        blob = bucket.blob("restaurants/" + image.filename)
-        blob.upload_from_string(
-            image.read(),
-            content_type=image.content_type)
         menu['image'] = image.filename
 
         datastore_client.put(menu)
