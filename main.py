38aea84ad82f3b3f06f2c535978b3610f89640ec
Mikołaj Herzberg <mikolaj.herzberg@gmail.com>
2022-12-10 16:36:00 +0100

    json loads

--- main.py
+++ main.py
@@ -300,16 +300,31 @@ def is_image_food():
         else:
             return 'Invalid format of a file', 400
 
+def add_food_to_restaurant_func(item, restaurant_name_key):
+    # Save Food entity to Datastore
+    restaurant_key = datastore_client.key("Food")
+
+    menu = datastore.Entity(key=restaurant_key)
+    menu['name'] = item['name']
+    menu['description'] = item['description']
+    menu['price'] = item['price']
+    menu['restaurantKey'] = restaurant_name_key
+    datastore_client.put(menu)
 
 def callback(message):
     # print(f'Received message: {message}')
-    menuObject = json.loads(message.data)
-    for item in menuObject["menu"]:
-        print(item)
-        print(type(item))
-        print(item['price'])
-        print(item['name'])
-        print(item['description'])
+    menu_object = json.loads(message.data)
+    restaurant_name = menu_object["restaurant"][0:-4]
+    print(f'restaurant {restaurant_name}')
+    for item in menu_object["menu"]:
+        restaurant_key = datastore_client.key('Restaurant', restaurant_name)
+        restaurant_entity = datastore_client.get(restaurant_key)
+        if restaurant_entity is None:
+            new_restaurant = datastore.Entity(key=restaurant_key)
+            new_restaurant['name'] = restaurant_name
+            datastore_client.put(new_restaurant)
+        restaurant_name_key = datastore_client.key("Restaurant", restaurant_name)
+        add_food_to_restaurant_func(item, restaurant_name_key)
     message.ack()
 
 
